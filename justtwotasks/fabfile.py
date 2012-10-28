25636d1b8763bc211861afee1f83ed0a5ccabf09
Lex Toumbourou <lextoumbourou@gmail.com>
2012-10-28 23:32:43 +1100

    Cleaned up fabfile slightly

--- justtwotasks/fabfile.py
+++ justtwotasks/fabfile.py
@@ -15,7 +15,7 @@ def deploy():
         if run('test -d {0}'.format(private.APP_DIR)).failed:
             run('git clone {0} {1}'.format(git_repo, private.APP_DIR))
     # Ensure I own the directory
-    sudo("chown -R " + private.USER_GROUP + " " + private.APP_DIR)
+    sudo("chown -R {0} {1}".format(private.USER_GROUP, private.APP_DIR))
     # Django app deployment tasks 
     with cd(private.APP_DIR):
         run('git pull')
