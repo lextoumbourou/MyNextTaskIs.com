1542aebf03971c15019a9670927fddaffd856d04
Lex Toumbourou <lextoumbourou@gmail.com>
2013-03-02 20:47:50 +1100

    More interface work

--- justtwotasks/templates/static/js/tasks.js
+++ justtwotasks/templates/static/js/tasks.js
@@ -3,56 +3,29 @@ function Task(data) {
     this.pk = ko.observable(data.pk);
     this.task = ko.observable(data.fields.task);
     this.is_complete = ko.observable(data.fields.is_complete);
-    this.delete_task = function() {
-        $.ajax("/task/"+self.pk(), {
-            type: "delete",
-            success: function() {
-                self.is_complete(false);
-                self.task("");
-            },
-        });
-    };
 
-    self.complete_task = function() {
+    this.complete_task = function() {
         self.is_complete(true);
     };
 }
 
 function TaskListViewModel() {
     var self = this;
-
-    self.completed_tasks = ko.observableArray([]);
-    self.incomplete_task = ko.observableArray([]);
-    self.date = $("#page_date").val();
-
-    $.getJSON("/get_tasks?&date="+self.date, function(allData) {
-        self.assign_to_arrays(allData);
-    });
-
-    self.save = function() {
-        console.log(ko.toJSON(self.incomplete_task()[0]));
-        $.ajax("/update_task?date="+self.date, {
-            data: ko.toJSON(self.incomplete_task()[0]),
-            type: "post", 
-            dataType: "json",
-            success: function(allData) {
-                self.assign_to_arrays(allData);
-            },
-        });
+    this.empty_task = function() {
+        return new Task({pk: 0, fields: { task:"", is_complete:false }});
     };
 
-    self.assign_to_arrays = function(allData) {
-        // Due to the way the backend works, 
-        // the first task will always be the incomplete
-        if ($.isEmptyObject(allData) || allData[0].is_complete) {
-            task = [new Task({pk: 0, fields: { task:"", is_complete:false }})];
-        }
-        else {
-            task = [new Task(allData[0])];
-        }
-        self.incomplete_task(task);
+    this.completed_tasks = ko.observableArray([]);
+    this.incomplete_task = ko.observable(self.empty_task());
+    this.date = $("#page_date").val();
+    this.all_data = {};
 
+    $.getJSON('/api/task', function(allData) {
         if (!$.isEmptyObject(allData)) {
+            if (!allData[0].fields.is_complete) {
+                self.incomplete_task(new Task(allData[0]));
+            }
+
             var mapped_tasks = $.map(allData, function(item) {
                 if (item.fields.is_complete) {
                     return new Task(item)
@@ -60,13 +33,54 @@ function TaskListViewModel() {
             });
             self.completed_tasks(mapped_tasks);
         }
+    });
+
+    this.delete_task = function(task) {
+        if (task.pk()) {
+            $.ajax("/api/task/"+task.pk(), {
+                type: "delete",
+                success: function() {
+                    self.incomplete_task(self.empty_task());
+                },
+            });
+        };
     };
 
-    self.clear_task = function() {
-        self.incomplete_task(
-            [new Task({pk: 0, fields: { task:"", is_complete:false }})]);
+    this.save = function(task) {
+        url = '/api/task/'
+        if (task.pk() != 0) {
+            url = url + task.pk()
+        }
+
+        $.ajax(url, {
+            data: ko.toJSON(
+                {'task': task.task(), 'is_complete': task.is_complete(),}),
+            type: "post", 
+            dataType: "json",
+            success: function(allData) {
+                if (task.is_complete()) {
+                    
+                }
+                task.pk(allData[0].pk);
+            },
+        });
     };
+
+    this.update_completed = function(task) {
+        self.completed_tasks.push(task);
+        self.incomplete_task(self.empty_task());
+    };
+
+     // Animation callbacks for the planets list
+    this.show_task_element = function(elem) { 
+        if (elem.nodeType === 1) $(elem).hide().fadeIn() 
+    }
+
+    this.hide_task_element = function(elem) { 
+        if (elem.nodeType === 1) $(elem).fadeOut(function() { $(elem).remove(); }) 
+    }
 };
+
 
 var task_list_model = new TaskListViewModel()
 ko.applyBindings(task_list_model);
