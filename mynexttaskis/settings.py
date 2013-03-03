297829418c6b5dac9f9eff018ef21407fe81106b
Lex Toumbourou <lextoumbourou@gmail.com>
2013-03-03 23:16:36 +1100

    Added south as dep

--- mynexttaskis/settings.py
+++ mynexttaskis/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'south'
     'bootup',
     'google_analytics',
     'lazysignup',
