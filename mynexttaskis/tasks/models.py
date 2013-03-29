4afb956d63e0fb77eebf227c8739ad65d0d2251a
Lex Toumbourou <lextoumbourou@gmail.com>
2013-03-29 12:26:55 +1100

    Added is_in_progress field

--- mynexttaskis/tasks/models.py
+++ mynexttaskis/tasks/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     user = models.ForeignKey(User)
     task = models.CharField(max_length=200)
     is_complete = models.BooleanField()
+    is_playing = models.BooleanField()
     created = models.DateField()
     start_time = models.DateField(blank=True, null=True)
     end_time = models.DateField(blank=True, null=True)
