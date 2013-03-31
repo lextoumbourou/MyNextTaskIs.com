4cf10aae3f0928fda777e26a3ab20030e479705a
Lex Toumbourou <lextoumbourou@gmail.com>
2013-03-31 13:33:15 +1100

    Buttons fade in when task is typed

--- mynexttaskis/templates/static/js/tasks.js
+++ mynexttaskis/templates/static/js/tasks.js
@@ -219,6 +219,11 @@ function TaskListViewModel() {
         self.in_progress_task(self.empty_task());
     };
 
+    self.fade_in_element = function(element, index, data) {
+        console.log(element);
+        $(element).fadeIn();
+    };
+
      // Animation callbacks for the planets list
     self.show_task_element = function(elem) { 
         if (elem.nodeType === 1) $(elem).hide().fadeIn() 
@@ -314,6 +319,21 @@ function TaskListViewModel() {
         );
     });
     self.sammy.run();
+};
+
+// Here's a custom Knockout binding that makes elements shown/hidden via jQuery's fadeIn()/fadeOut() methods
+// Could be stored in a separate utility library
+ko.bindingHandlers.fadeVisible = {
+    init: function(element, valueAccessor) {
+        // Initially set the element to be instantly visible/hidden depending on the value
+        var value = valueAccessor();
+        $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+    },
+    update: function(element, valueAccessor) {
+        // Whenever the value subsequently changes, slowly fade the element in or out
+        var value = valueAccessor();
+        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+    }
 };
 
 var task_list_model = new TaskListViewModel()
